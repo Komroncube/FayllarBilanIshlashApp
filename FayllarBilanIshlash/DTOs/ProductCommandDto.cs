475964755236ff62346449351c595cad403982f6
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-01 14:40:59 +0500

    productdtos

---
+++ FayllarBilanIshlash/DTOs/ProductCommandDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FayllarBilanIshlash.DTOs
+{
+    public class ProductCommandDto
+    {
+        public string Name { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
---
+++ FayllarBilanIshlash/DTOs/ProductResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FayllarBilanIshlash.DTOs
+{
+    public class ProductResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
