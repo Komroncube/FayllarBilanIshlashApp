cdd1bd148e9e31c0af9092a1b481785e5902d5fc
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-01 14:40:30 +0500

    dbContext added

---
+++ FayllarBilanIshlash/Data/ProductDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FayllarBilanIshlash.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FayllarBilanIshlash.Data
+{
+    public class ProductDbContext : DbContext 
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
