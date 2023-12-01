4ebfdb6ec133a2c5b52d24a39e2a8299871a2f60
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-01 14:41:33 +0500

    productService added

---
+++ FayllarBilanIshlash/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using FayllarBilanIshlash.DTOs;
+
+namespace FayllarBilanIshlash.Services
+{
+    public interface IProductService
+    {
+        public void CreateProduct(ProductCommandDto productCommandDto);
+        public ProductResponseDto GetProductWithImage(int Id);
+        public ProductResponseDto GetProduct(int Id);
+    }
+}
---
+++ FayllarBilanIshlash/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using FayllarBilanIshlash.Data;
+using FayllarBilanIshlash.DTOs;
+using FayllarBilanIshlash.Models;
+
+namespace FayllarBilanIshlash.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ProductDbContext productDbContext;
+        private readonly IFileService _fileService;
+        public ProductService(ProductDbContext productDbContext, IFileService fileService)
+        {
+            this.productDbContext = productDbContext;
+            _fileService = fileService;
+        }
+
+        public void CreateProduct(ProductCommandDto productCommandDto)
+        {
+            var product = new Product();
+            product.Name = productCommandDto.Name;
+            product.FilePath = _fileService.UploadImage(productCommandDto.Image);
+            productDbContext.Products.Add(product);
+            productDbContext.SaveChanges();
+
+
+        }
+
+        public ProductResponseDto GetProduct(int Id)
+        {
+            Product product = productDbContext.Products.FirstOrDefault(x => x.Id == Id);
+            var response = new ProductResponseDto() { Id = product.Id, Name = product.Name };
+            return response;
+        }
+
+        public ProductResponseDto GetProductWithImage(int Id)
+        {
+            Product product = productDbContext.Products.FirstOrDefault(x => x.Id == Id);
+            if (product is null)
+                return new ProductResponseDto();
+
+            var response = new ProductResponseDto()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Image = _fileService.DownloadImage(product.FilePath)
+            };
+            return response;
+        }
+    }
+}
