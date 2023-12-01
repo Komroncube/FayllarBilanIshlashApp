827d9072092b64d5a867733cb229317ee196a52c
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-01 14:41:46 +0500

    controller for product added

---
+++ FayllarBilanIshlash/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using FayllarBilanIshlash.Data;
+using FayllarBilanIshlash.DTOs;
+using FayllarBilanIshlash.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FayllarBilanIshlash.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public FileContentResult GetImage(int id)
+        {
+            ProductResponseDto product = _productService.GetProductWithImage(id);
+
+            return File(product.Image, "image/png");
+        }
+        [HttpPost]
+        public void CreateProduct([FromForm] ProductCommandDto productCommandDto)
+        {
+            _productService.CreateProduct(productCommandDto);
+        }
+        [HttpGet]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _productService.GetProduct(id);
+            return Ok(product);
+        }
+
+    }
+}
